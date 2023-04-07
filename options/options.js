8860984c3923ff6274def69ce2dfba05cc43ab38
Crowdbotics <team@crowdbotics.com>
2023-04-07 22:02:17 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "throbbing-hill-39783",
+  url: "https://throbbing-hill-39783.botics.co",
+  api: "https://throbbing-hill-39783.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
